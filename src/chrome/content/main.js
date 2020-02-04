19673cb2683b7d547f328d94e4b72ca9c95785d2
asd <zhuzemin4@hotmail.com>
2020-02-04 08:26:48 +0800

    add: title color change when header change

--- src/chrome/content/main.js
+++ src/chrome/content/main.js
@@ -1,28 +1,81 @@
-    var lastTabLabel;
     var imgList=[];
-    var imagePath=getComplexValue('Header_Image_Switch_Path')||null;
+    var imagePath;
+    try{
+        imagePath=getComplexValue('Header_Image_Switch_Path');
+    }
+    catch (e) {
+        imagePath=null;
+    }
+    var speed;
+    try{
+        speed=getComplexValue('Header_Image_Switch_Speed');
+    }
+    catch (e) {
+        speed=200;
+    }
+    var hex=new Array("00","14","28","3C","50","64","78","8C","A0","B4","C8","DC","F0")
+    var r=1
+    var g=1
+    var b=1
+    var seq=1
+    var entensionEnable;
+    try{
+        entensionEnable=getBoolPref('Header_Image_Switch_Enable');
+    }
+    catch (e) {
+        entensionEnable=true;
+    }
+    var main_window=document.querySelector('#main-window');
 main();
 function main(){
-	if(imagePath!=null){
-    readDir(imagePath);
+	if(imagePath!=null&&entensionEnable){
+        readDir(imagePath);
     //readDir(OS.Constants.Path.profileDir + '\\chrome\\header image');
-    var main_window=document.querySelector('#main-window');
-    main_window.querySelector('#TabsToolbar').addEventListener("click", function () {
-        var currentTabLabel=window.gBrowser.selectedTab.label;
-        if(currentTabLabel!=lastTabLabel){
-            var randomNum = Math.floor(Math.random() * (imgList.length - 0));
-            var img = imgList[randomNum];
-            main_window.style.setProperty('--lwt-header-image', 'url("' + encodeURI('file:///' + img.replace(/\\/g, '/')) + '")', 'important');
-            main_window.style.setProperty('--lwt-background-alignment','TOP');
-            lastTabLabel=currentTabLabel;
+        window.gBrowser.addEventListener("select",  function () {
+            if(entensionEnable){
+                var randomNum = Math.floor(Math.random() * (imgList.length - 0));
+                var img = imgList[randomNum];
+                main_window.style.setProperty('--lwt-header-image', 'url("' + encodeURI('file:///' + img.replace(/\\/g, '/')) + '")', 'important');
+                main_window.style.setProperty('--lwt-background-alignment','TOP');                change();
+                //var rainbow="#"+hex[r]+hex[g]+hex[b]
+                main_window.querySelector('#tabbrowser-tabs').style.setProperty('-webkit-text-fill-color', getRandomColor(), 'important');
+
+
+            }
+   });
+        /*var interval=setInterval(function () {
+            if(entensionEnable){
+                change();
+                var rainbow="#"+hex[r]+hex[g]+hex[b]
+                main_window.querySelector('#tabbrowser-tabs').style.setProperty('-webkit-text-fill-color', rainbow, 'important');
+                i++;
+            }
+            else{
+                clearInterval(interval);
+
+            }
+        },speed);*/
+}
+}
+
+function entensionEnableSwitch(){
+        if(entensionEnable){
+            entensionEnable=false;
+            main_window.querySelector('#tabbrowser-tabs').style.setProperty('-webkit-text-fill-color', 'black', 'important');
+            main_window.querySelector('#Header_Image_Switch_btn').className='disable';
         }
-    });
-}
-}
+        else {
+            entensionEnable=true;
+            main();
+            main_window.querySelector('#Header_Image_Switch_btn').className='enable';
+
+        }
+        setBoolPref('Header_Image_Switch_Enable',entensionEnable);
+    }
     function readDir(path) {
         var arr = readDirectory(path);
         for (var item of arr) {
-            if (item.includes('.')) {
+            if (/\.(jpg|png|gif|bmp)$/.test(item)) {
                 imgList.push(path + '\\' + item);
             }
             else {
@@ -48,7 +101,7 @@ function main(){
 
             while (children.hasMoreElements()) {
 
-                child = children.getNext().QueryInterface(Components.interfaces.nsILocalFile);
+                var child = children.getNext().QueryInterface(Components.interfaces.nsILocalFile);
 
                 list.push(child.leafName);
 
@@ -65,7 +118,7 @@ var prompts = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
 
 var check = {value: false};                  // default the checkbox to false
 
-var imagePathObj = {value: 'C:\\Header_image'};                  // default the edit field to Bob
+var imagePathObj = {value: imagePath};                  // default the edit field to Bob
 
 var result = prompts.prompt(null, "Header Image Switch", "Image folder path:", imagePathObj, null, check);
 if(result){
@@ -74,6 +127,21 @@ if(result){
 	main();
 }
 }
+    function setTitleColorChangeSpeed(){
+        var prompts = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+            .getService(Components.interfaces.nsIPromptService);
+
+        var check = {value: false};                  // default the checkbox to false
+
+        var obj = {value: speed};                  // default the edit field to Bob
+
+        var result = prompts.prompt(null, "Header Image Switch", "Title Color Change Speed:", obj, null, check);
+        if(result){
+            speed=obj.value;
+            setComplexValue('Header_Image_Switch_Speed',speed);
+            main();
+        }
+    }
 //Read Preference Item
 function getComplexValue(Varible)
 	{
@@ -88,4 +156,96 @@ function setComplexValue(Varible,Value)
 	var str = Components.classes["@mozilla.org/supports-string;1"].createInstance(Components.interfaces.nsISupportsString);
 	str.data = Value;
 	prefb.setComplexValue(Varible, Components.interfaces.nsISupportsString, str);
-	}	
+	}
+
+    function change(){
+        if (seq==6){
+            b--
+            if (b==0)
+                seq=1
+        }
+        if (seq==5){
+            r++
+            if (r==12)
+                seq=6
+        }
+        if (seq==4){
+            g--
+            if (g==0)
+                seq=5
+        }
+        if (seq==3){
+            b++
+            if (b==12)
+                seq=4
+        }
+        if (seq==2){
+            r--
+            if (r==0)
+                seq=3
+        }
+        if (seq==1){
+            g++
+            if (g==12)
+                seq=2
+        }
+    }
+    //Get Boolean Preference
+    function getBoolPref(Varible)
+    {
+        var prefb = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+        var str = prefb.getBoolPref(Varible);
+        return str
+    }
+    //Set Boolean Preference
+    function setBoolPref(Varible,Value)
+    {
+        var prefb = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
+        var str = Components.classes["@mozilla.org/supports-string;1"].createInstance(Components.interfaces.nsISupportsString);
+        prefb.setBoolPref(Varible, Value);
+    }
+    function getRandomColor() {
+        var Colors = {};
+        Colors.names = {
+            aqua: "#00ffff",
+            azure: "#f0ffff",
+            beige: "#f5f5dc",
+            black: "#000000",
+            blue: "#0000ff",
+            brown: "#a52a2a",
+            cyan: "#00ffff",
+            fuchsia: "#ff00ff",
+            gold: "#ffd700",
+            green: "#008000",
+            indigo: "#4b0082",
+            khaki: "#f0e68c",
+            lightblue: "#add8e6",
+            lightcyan: "#e0ffff",
+            lightgreen: "#90ee90",
+            lightgrey: "#d3d3d3",
+            lightpink: "#ffb6c1",
+            lightyellow: "#ffffe0",
+            lime: "#00ff00",
+            magenta: "#ff00ff",
+            maroon: "#800000",
+            navy: "#000080",
+            olive: "#808000",
+            orange: "#ffa500",
+            pink: "#ffc0cb",
+            purple: "#800080",
+            violet: "#800080",
+            red: "#ff0000",
+            silver: "#c0c0c0",
+            white: "#ffffff",
+            yellow: "#ffff00"
+        };
+        Colors.random = function() {
+            var result;
+            var count = 0;
+            for (var prop in this.names)
+                if (Math.random() < 1/++count)
+                    result = prop;
+            return result;
+        };
+        return Colors.names[Colors.random()];
+    }
